cb6865ddcaeb49cf7a82506c771fa6b51f2306bc
Thibault Deutsch <thibault.deutsch@gmail.com>
2014-02-01 01:02:06 +0100

    add SFXManager.cs

---
+++ src/Arrow/Arrow/Map/SFXManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Arrow.Map
+{
+    static class SFXManager
+    {
+        private static Dictionary<string, SoundEffect> soundEffects =
+            new Dictionary<string, SoundEffect>();
+
+        public static void AddSFX(string name, SoundEffect effect)
+        {
+            soundEffects.Add(name, effect);
+        }
+
+        public static void Play(string name)
+        {
+            if (soundEffects.ContainsKey(name))
+                soundEffects[name].Play();
+        }
+    }
+}
