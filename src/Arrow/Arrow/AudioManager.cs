eb8bdfa8011d310c378dbc586cc0e1d133b23745
Rémy Bernier <remyhandball@hotmail.fr>
2014-02-18 01:55:24 +0100

    Classe Son

---
+++ src/Arrow/Arrow/AudioManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace Arrow
+{
+    class AudioManager
+    {
+    }
+}
