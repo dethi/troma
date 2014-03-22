616eca8c41e17345ef51a680ef3d542746bc0b6a
Thibault Deutsch <thibault.deutsch@gmail.com>
2014-03-23 00:14:14 +0100

    start InputState

---
+++ src/Arrow/Arrow/Input/InputAction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Arrow
+{
+    public partial class InputState
+    {
+        public Vector3 PlayerMove()
+        {
+            Vector3 moveVector = Vector3.Zero;
+
+            if (isGamePadConnected)
+            {
+                if (CurrentGamePadState.ThumbSticks.Left.X != 0)
+                    moveVector.X -= CurrentGamePadState.ThumbSticks.Left.X;
+                if (CurrentGamePadState.ThumbSticks.Left.Y != 0)
+                    moveVector.Z += CurrentGamePadState.ThumbSticks.Left.Y;
+            }
+            else
+            {
+                if (IsDown(mapping.KB_UP))
+                    moveVector.Z += 1;
+                if (IsDown(mapping.KB_BOTTOM))
+                    moveVector.Z += -1;
+                if (IsDown(mapping.KB_LEFT))
+                    moveVector.X += 1;
+                if (IsDown(mapping.KB_RIGHT))
+                    moveVector.X += -1;
+            }
+
+            if (moveVector != Vector3.Zero)
+                moveVector.Normalize();
+
+            return moveVector;
+        }
+    }
+}
---
+++ src/Arrow/Arrow/Input/InputConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Arrow
+{
+    public class InputConfiguration
+    {
+        #if BUILD
+
+        // AZERTY KEYBOARD
+        public readonly Keys KB_UP = Keys.Z;
+        public readonly Keys KB_BOTTOM = Keys.S;
+        public readonly Keys KB_LEFT = Keys.Q;
+        public readonly Keys KB_RIGHT = Keys.D;
+        
+        #else
+
+        // QWERTY KEYBOARD
+        public readonly Keys KB_UP = Keys.W;
+        public readonly Keys KB_BOTTOM = Keys.S;
+        public readonly Keys KB_LEFT = Keys.A;
+        public readonly Keys KB_RIGHT = Keys.D;
+
+        #endif
+
+        public readonly Keys KB_CROUCH = Keys.LeftControl;
+        public readonly Keys KB_RUN = Keys.LeftShift;
+        public readonly Keys KB_JUMP = Keys.Space;
+        public readonly Keys KB_RELOAD = Keys.R;
+    }
+}
