2d6b3f7ed7ea859cf2cf91e72c65f72b993812be
Marc Fresne <pignol.fresne@gmail.com>
2014-04-08 21:28:42 +0200

    Quelques modèles sans texture
    La classe "gameplayscreen" a été modifié a l'arrache :p

--- src/Arrow/Arrow/Screens/GameplayScreen.cs
+++ src/Arrow/Arrow/Screens/GameplayScreen.cs
@@ -25,6 +25,8 @@ namespace Arrow
         private EntityManager entities;
         private Texture2D cross;
 
+        private string mode_jeu;
+
         private float pauseAlpha;
 
         #endregion
@@ -39,6 +41,7 @@ namespace Arrow
 
             camera = Camera.Instance;
             player = new Player(game, new Vector3(80, 10, 460));
+            mode_jeu = "Town";
         }
 
         public override void LoadContent()
@@ -53,8 +56,8 @@ namespace Arrow
 
             mapEffect = content.Load<Effect>("Effects/Terrain");
             maps = new MapManager(game,
-                "Textures/Farm/heightmap",
-                "Textures/Farm/texture",
+                "Textures/" + mode_jeu + "/heightmap",
+                "Textures/" + mode_jeu + "/texture",
                 513f,
                 513,
                 513,
@@ -64,122 +67,166 @@ namespace Arrow
 
             entities = new EntityManager(game, maps);
 
-            skydome = new Entity(game, "skydome", new Vector3(
-                256, 
-                maps.GetHeight(256, 256).Value, 
-                256));
-            skydome.lightingEnabled = false;
+            if (mode_jeu == "Farm")
+            {
+                
+                skydome = new Entity(game, "skydome", new Vector3(
+                    256,
+                    maps.GetHeight(256, 256).Value,
+                    256));
+                skydome.lightingEnabled = false;
 
-            #region Models
+                #region Models
 
-            #region Wood barrier
+                #region Wood barrier
 
-            Model woodBarrierX = content.Load<Model>("Models/wood_barrier_x");
-            Model woodBarrierZ = content.Load<Model>("Models/wood_barrier_z");
+                Model woodBarrierX = content.Load<Model>("Models/wood_barrier_x");
+                Model woodBarrierZ = content.Load<Model>("Models/wood_barrier_z");
 
-            entities.AddEntity(woodBarrierZ, new Vector2(119, 101));
-            entities.AddEntity(woodBarrierZ, new Vector2(119, 91));
-            entities.AddEntity(woodBarrierZ, new Vector2(119, 81));
-            entities.AddEntity(woodBarrierZ, new Vector2(119, 71));
-            entities.AddEntity(woodBarrierZ, new Vector2(119, 61));
-            entities.AddEntity(woodBarrierZ, new Vector2(119, 51));
-            entities.AddEntity(woodBarrierZ, new Vector2(119, 41));
-            entities.AddEntity(woodBarrierZ, new Vector2(119, 31));
-            entities.AddEntity(woodBarrierZ, new Vector2(119, 21));
-            entities.AddEntity(woodBarrierZ, new Vector2(119, 11));
-            entities.AddEntity(woodBarrierZ, new Vector2(119, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(109, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(99, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(89, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(79, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(69, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(59, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(49, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(39, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(29, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(19, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(9, 1));
-            entities.AddEntity(woodBarrierX, new Vector2(0, 1));
+                entities.AddEntity(woodBarrierZ, new Vector2(119, 101));
+                entities.AddEntity(woodBarrierZ, new Vector2(119, 91));
+                entities.AddEntity(woodBarrierZ, new Vector2(119, 81));
+                entities.AddEntity(woodBarrierZ, new Vector2(119, 71));
+                entities.AddEntity(woodBarrierZ, new Vector2(119, 61));
+                entities.AddEntity(woodBarrierZ, new Vector2(119, 51));
+                entities.AddEntity(woodBarrierZ, new Vector2(119, 41));
+                entities.AddEntity(woodBarrierZ, new Vector2(119, 31));
+                entities.AddEntity(woodBarrierZ, new Vector2(119, 21));
+                entities.AddEntity(woodBarrierZ, new Vector2(119, 11));
+                entities.AddEntity(woodBarrierZ, new Vector2(119, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(109, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(99, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(89, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(79, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(69, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(59, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(49, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(39, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(29, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(19, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(9, 1));
+                entities.AddEntity(woodBarrierX, new Vector2(0, 1));
 
-            #endregion
+                #endregion
 
-            #region Barbed barrier
+                #region Barbed barrier
 
-            Model barbedBarrierZ = content.Load<Model>("Models/barbed_barrier_z");
-            Model postBarrier = content.Load<Model>("Models/post_barrier");
+                Model barbedBarrierZ = content.Load<Model>("Models/barbed_barrier_z");
+                Model postBarrier = content.Load<Model>("Models/post_barrier");
 
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 0));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 9));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 18));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 27));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 36));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 45));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 54));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 63));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 72));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 81));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 90));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 99));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 108));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 117));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 126));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 135));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 144));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 153));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 162));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 171));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 180));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 189));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 198));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 207));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 216));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 225));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 234));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 242));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 251));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 260));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 269));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 278));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 287));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 296));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 305));
-            entities.AddEntity(postBarrier, new Vector2(160, 315));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 0));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 9));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 18));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 27));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 36));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 45));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 54));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 63));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 72));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 81));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 90));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 99));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 108));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 117));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 126));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 135));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 144));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 153));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 162));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 171));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 180));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 189));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 198));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 207));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 216));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 225));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 234));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 242));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 251));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 260));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 269));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 278));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 287));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 296));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 305));
+                entities.AddEntity(postBarrier, new Vector2(160, 315));
 
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 340));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 349));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 358));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 367));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 376));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 385));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 394));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 403));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 412));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 421));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 430));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 439));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 448));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 457));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 466));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 475));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 484));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 493));
-            entities.AddEntity(barbedBarrierZ, new Vector2(160, 502));
-            entities.AddEntity(postBarrier, new Vector2(160, 511));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 340));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 349));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 358));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 367));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 376));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 385));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 394));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 403));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 412));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 421));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 430));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 439));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 448));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 457));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 466));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 475));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 484));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 493));
+                entities.AddEntity(barbedBarrierZ, new Vector2(160, 502));
+                entities.AddEntity(postBarrier, new Vector2(160, 511));
 
-            #endregion
+                #endregion
 
-            entities.AddEntity("house", new Vector2(0, 511));
-            entities.AddEntity("barn", new Vector2(60, 511));
-            entities.AddEntity("bandbags", new Vector2(150, 150));
-            entities.AddEntity("table", new Vector2(58, 400));
-            entities.AddEntity("barrel", new Vector2(58, 405));
-            entities.AddEntity("truck_allemand", new Vector2(122, 206));
-            entities.AddEntity("farm", new Vector2(0, 262));
-            entities.AddEntity("shelter", new Vector2(0, 411));
+                entities.AddEntity("house", new Vector2(0, 511));
+                entities.AddEntity("barn", new Vector2(60, 511));
+                entities.AddEntity("bandbags", new Vector2(150, 150));
+                entities.AddEntity("table", new Vector2(58, 400));
+                entities.AddEntity("barrel", new Vector2(58, 405));
+                entities.AddEntity("truck_allemand", new Vector2(122, 206));
+                entities.AddEntity("farm", new Vector2(0, 262));
+                entities.AddEntity("shelter", new Vector2(0, 411));
 
-            #endregion
+                #endregion
 
-            ScreenManager.Game.ResetElapsedTime();
+                ScreenManager.Game.ResetElapsedTime();
+            }
+            else
+            {
+                entities.AddEntity("Town/gare", new Vector2(0, 0));
+                entities.AddEntity("Town/batiment1", new Vector2(256, 256));
+                entities.AddEntity("Town/batiment2", new Vector2(100, 100));
+                entities.AddEntity("Town/batiment3", new Vector2(400, 400));
+
+                Model railX = content.Load<Model>("Models/Town/rail");
+                entities.AddEntity(railX, new Vector2(0, 775));
+                entities.AddEntity(railX, new Vector2(120, 775));
+                entities.AddEntity(railX, new Vector2(240, 775));
+                entities.AddEntity(railX, new Vector2(360, 775));
+                entities.AddEntity(railX, new Vector2(480, 775));
+                entities.AddEntity(railX, new Vector2(600, 775));
+                entities.AddEntity(railX, new Vector2(720, 775));
+                entities.AddEntity(railX, new Vector2(840, 775));
+                entities.AddEntity(railX, new Vector2(960, 775));
+                entities.AddEntity(railX, new Vector2(1080, 775));
+                entities.AddEntity(railX, new Vector2(1200, 775));
+                entities.AddEntity(railX, new Vector2(1320, 775));
+                entities.AddEntity(railX, new Vector2(1440, 775));
+                entities.AddEntity(railX, new Vector2(1560, 775));
+                entities.AddEntity(railX, new Vector2(1680, 775));
+
+                entities.AddEntity(railX, new Vector2(0, 784));
+                entities.AddEntity(railX, new Vector2(120, 784));
+                entities.AddEntity(railX, new Vector2(240, 784));
+                entities.AddEntity(railX, new Vector2(360, 784));
+                entities.AddEntity(railX, new Vector2(480, 784));
+                entities.AddEntity(railX, new Vector2(600, 784));
+                entities.AddEntity(railX, new Vector2(720, 784));
+                entities.AddEntity(railX, new Vector2(840, 784));
+                entities.AddEntity(railX, new Vector2(960, 784));
+                entities.AddEntity(railX, new Vector2(1080, 784));
+                entities.AddEntity(railX, new Vector2(1200, 784));
+                entities.AddEntity(railX, new Vector2(1320, 784));
+                entities.AddEntity(railX, new Vector2(1440, 784));
+                entities.AddEntity(railX, new Vector2(1560, 784));
+                entities.AddEntity(railX, new Vector2(1680, 784));
+            }
         }
 
         /// <summary>
@@ -213,7 +260,7 @@ namespace Arrow
         public override void Draw(GameTime gameTime)
         {
             maps.Draw(mapEffect);
-            skydome.Draw();
+            //skydome.Draw();
             entities.Draw();
             player.Draw();
 
