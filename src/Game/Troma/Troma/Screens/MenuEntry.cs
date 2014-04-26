f1223bb667bd961b7fec926a6bb7664841a130be
Thibault Deutsch <thibault.deutsch@gmail.com>
2014-04-26 02:46:24 +0200

    clean code

--- src/Game/Troma/Troma/Screens/MenuEntry.cs
+++ src/Game/Troma/Troma/Screens/MenuEntry.cs
@@ -13,8 +13,8 @@ namespace Troma
         public string Text;
         public Vector2 Position;
 
-        float selectionFade;
-        readonly Color notSelectedColor;
+        private float selectionFade;
+        private readonly Color notSelectedColor;
 
         public event EventHandler Selected;
 
@@ -24,57 +24,45 @@ namespace Troma
             notSelectedColor = new Color(150, 145, 140);
         }
 
-        public virtual void Update(MenuScreen screen, bool isSelected, GameTime gameTime)
+        public virtual void Update(GameTime gameTime, MenuScreen screen, bool isSelected)
         {
-            float fadeSpeed = (float)gameTime.ElapsedGameTime.TotalSeconds * 4;
-
-            if (isSelected)
-                selectionFade = 1;
-            else
-                selectionFade = 0;
+            selectionFade = (isSelected) ? 1 : 0;
         }
 
-        public virtual void Draw(MenuScreen screen, bool isSelected, GameTime gameTime)
+        public virtual void Draw(GameTime gameTime, MenuScreen screen, bool isSelected)
         {
-
             // Pulsate the size of the selected menu entry.            
-            double time = gameTime.TotalGameTime.TotalSeconds;
-            float pulsate = (float)Math.Sin(time * 6) + 1;
+            float pulsate = (float)Math.Sin(gameTime.TotalGameTime.TotalSeconds * 6) + 1;
 
-            float r = 64 * pulsate / 700;
-            float g = 56 * pulsate / 700;
-            float b = 48 * pulsate / 700;
-            Color selectedColor = new Color(r, g, b);
+            Color selectedColor = new Color(
+                64 * pulsate / 700, 
+                56 * pulsate / 700, 
+                48 * pulsate / 700);
 
-            Color color = isSelected ? selectedColor : notSelectedColor;
+            Color color = (isSelected) ? selectedColor : notSelectedColor;
+            color *= screen.TransitionAlpha;
+
             float scale = ((0.75f + selectionFade * 0.5f + 
                 selectionFade * pulsate * 0.1f) * 
                 GameServices.GraphicsDevice.Viewport.Width) / 1980;
-            
-            // Modify the alpha to fade text out during transitions.
-            color *= screen.TransitionAlpha;
+            Vector2 origin = new Vector2(0, screen.Font.LineSpacing / 2);
 
-            // Draw text, centered on the middle of each line.
-            SpriteFont spriteFont = screen.Font;
-
-            Vector2 origin = new Vector2(0, spriteFont.LineSpacing / 2);
-
-            GameServices.SpriteBatch.DrawString(spriteFont, Text, Position, 
+            GameServices.SpriteBatch.DrawString(screen.Font, Text, Position, 
                 color, -0.35f, origin, scale, SpriteEffects.None, 0);
         }
 
-        protected internal virtual void OnSelectEntry()
+        protected internal void OnSelectEntry()
         {
             if (Selected != null)
                 Selected(this, new EventArgs());
         }
 
-        public virtual int GetHeight(MenuScreen screen)
+        public int GetHeight(MenuScreen screen)
         {
             return screen.Font.LineSpacing;
         }
 
-        public virtual int GetWidth(MenuScreen screen)
+        public int GetWidth(MenuScreen screen)
         {
             return (int)screen.Font.MeasureString(Text).X;
         }
