1711fd12c99c1e269897c92038b9fcc79007ce43
Anthony Belthier <belthi_a@epita.fr>
2014-05-14 23:02:30 +0200

    changement bouton

--- src/Game/Troma/Troma/Screens/MenuEntry.cs
+++ src/Game/Troma/Troma/Screens/MenuEntry.cs
@@ -21,6 +21,9 @@ namespace Troma
         private readonly Color notSelectedColor;
         private readonly Color selectedColor;
 
+        private readonly Color notSelectedColor2;
+        private readonly Color selectedColor2;
+
         private float pulsate;
 
         public event EventHandler Selected;
@@ -38,6 +41,9 @@ namespace Troma
 
             notSelectedColor = new Color(150, 145, 140);
             selectedColor = Color.Black;
+
+            notSelectedColor2 = new Color(150, 145, 140);
+            selectedColor2 = Color.Red;
         }
 
         public virtual void Update(GameTime gameTime, MenuScreen screen, bool isSelected)
@@ -56,11 +62,21 @@ namespace Troma
             Color color = (isSelected) ? selectedColor : notSelectedColor;
             color *= screen.TransitionAlpha;
 
+            Color color2 = (isSelected) ? selectedColor2 : notSelectedColor2;
+            color *= screen.TransitionAlpha;
+
             float scale = ((FontSize + selectionFade * 0.5f + 
                 selectionFade * pulsate * 0.1f) * 
                 GameServices.GraphicsDevice.Viewport.Width) / 1980;
+
+            float scale2 = ((FontSize + selectionFade * 0.52f +
+                selectionFade * pulsate * 0.1f) *
+                GameServices.GraphicsDevice.Viewport.Width) / 1980;
+
             Vector2 origin = new Vector2(0, screen.Font.LineSpacing / 2);
 
+            GameServices.SpriteBatch.DrawString(screen.Font, Text, Position, color2 * 0.2f, _rotation,
+                origin, scale2, SpriteEffects.None, 0);
             GameServices.SpriteBatch.DrawString(screen.Font, Text, Position, color, _rotation, 
                 origin, scale, SpriteEffects.None, 0);
         }
