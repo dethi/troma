382912e712b6f8e8f3f4ac6bb96de971a182b639
Thibault Deutsch <thibault.deutsch@gmail.com>
2014-06-09 12:34:16 +0200

    better help look

--- src/Game/Troma/Troma/Screens/MenuScreens/HelpMenu.cs
+++ src/Game/Troma/Troma/Screens/MenuScreens/HelpMenu.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using GameEngine;
 
 namespace Troma
@@ -13,12 +14,16 @@ namespace Troma
         private Entry backMenuEntry;
 
         private Texture2D bg;
+        private Texture2D bgTrans;
         private Texture2D arrow;
 
         private Rectangle bgRect;
+        private Rectangle bgTransRect;
         private Rectangle arrowRect;
 
+        private String keyLabel;
         private String key;
+        private Vector2 keyLabelPos;
         private Vector2 keyPos;
 
         public HelpMenu(Game game)
@@ -43,10 +48,14 @@ namespace Troma
             base.LoadContent();
 
             bg = FileManager.Load<Texture2D>("Menus/background-blur");
+            bgTrans = FileManager.Load<Texture2D>("Menus/translucide");
             arrow = FileManager.Load<Texture2D>("Menus/arrow");
 
             bgRect = new Rectangle(0, 0, 1920, 1080);
+            bgTransRect = new Rectangle(0, 0, 0, 0);
             arrowRect = new Rectangle(0, 0, 64, 64);
+
+            Dictionary<KeyActions, Keys> kb = InputConfiguration.GetKeybord();
 
             StringBuilder tmp = new StringBuilder();
             tmp.AppendLine(Resource.Up);
@@ -54,12 +63,26 @@ namespace Troma
             tmp.AppendLine(Resource.Left);
             tmp.AppendLine(Resource.Right);
             tmp.AppendLine(Resource.Run);
-            tmp.AppendLine(Resource.Aimfor);
-            tmp.AppendLine(Resource.Reload);
             tmp.AppendLine(Resource.Jump);
             tmp.AppendLine(Resource.Crouch);
-            tmp.AppendLine(Resource.Menu_Paused);
+            tmp.AppendLine(Resource.Reload);
             tmp.AppendLine(Resource.Shoot);
+            tmp.AppendLine(Resource.Aimfor);
+            tmp.AppendLine(Resource.MenuPaused);
+            keyLabel = tmp.ToString();
+
+            tmp.Clear();
+            tmp.AppendLine(kb[KeyActions.Up].ToString());
+            tmp.AppendLine(kb[KeyActions.Bottom].ToString());
+            tmp.AppendLine(kb[KeyActions.Left].ToString());
+            tmp.AppendLine(kb[KeyActions.Right].ToString());
+            tmp.AppendLine(kb[KeyActions.Run].ToString());
+            tmp.AppendLine(kb[KeyActions.Jump].ToString());
+            tmp.AppendLine(kb[KeyActions.Crouch].ToString());
+            tmp.AppendLine(kb[KeyActions.Reload].ToString());
+            tmp.AppendLine(Resource.LeftMouse);
+            tmp.AppendLine(Resource.RightMouse);
+            tmp.AppendLine(Keys.P.ToString());
             key = tmp.ToString();
         }
 
@@ -70,20 +93,41 @@ namespace Troma
 
             float widthScale = (float)width / 1920;
             float heightScale = (float)height / 1080;
+            float scale = (widthScale + heightScale) / 2;
 
             bgRect.Height = height;
             bgRect.Width = (int)(height * 1.778f);
             bgRect.X = -(bgRect.Width - width) / 2;
 
-            keyPos = new Vector2(
+            keyLabelPos = new Vector2(
                 143 * widthScale,
                 120 * heightScale);
+
+            keyPos = new Vector2(
+                keyLabelPos.X + SpriteFont.MeasureString(keyLabel).X * scale + 200 * widthScale,
+                120 * heightScale);
+
+            float lineSpacing = SpriteFont.LineSpacing * ((widthScale + heightScale) / 2);
+            int nbLine = keyLabel.Split('\n').Length - 1;
+
+            bgTransRect.X = (int)(133 * widthScale);
+            bgTransRect.Height = (int)(lineSpacing);
+            bgTransRect.Width = (int)(keyPos.X + (SpriteFont.MeasureString(key).X + 40) * scale);
 
             GameServices.SpriteBatch.Begin();
 
             GameServices.SpriteBatch.Draw(bg, bgRect, Color.White * TransitionAlpha);
+
+            for (float i = 0; i < nbLine; i += 2)
+            {
+                bgTransRect.Y = (int)(keyLabelPos.Y + i * lineSpacing);
+                GameServices.SpriteBatch.Draw(bgTrans, bgTransRect, Color.White * TransitionAlpha * 0.15f);
+            }
+
+            GameServices.SpriteBatch.DrawString(SpriteFont, keyLabel, keyLabelPos, Color.Black * TransitionAlpha, 0,
+                Vector2.Zero, scale, SpriteEffects.None, 0);
             GameServices.SpriteBatch.DrawString(SpriteFont, key, keyPos, Color.Black * TransitionAlpha, 0,
-                Vector2.Zero, (widthScale + heightScale) / 2, SpriteEffects.None, 0);
+                Vector2.Zero, scale, SpriteEffects.None, 0);
 
             // Draw each menu entry in turn.
             for (int i = 0; i < MenuEntries.Count; i++)
